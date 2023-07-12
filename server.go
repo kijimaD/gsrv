10677f97f5e1d457420be2df0da1a1b01daeafef
kijimaD <norimaking777@gmail.com>
2023-07-12 23:16:10 +0900

    feat: service追加した

--- server.go
+++ server.go
@@ -4,9 +4,7 @@ import (
 	"io"
 )
 
-func service(in *io.Reader, out *io.Writer, docroot string) {
-	// req := readRequest(in)
-	// respondTo(req, out, docroot)
-
-	// fmt.Println(out)
+func service(in io.Reader, out io.Writer, docroot string) {
+	req := readRequest(in)
+	respondTo(req, out, docroot)
 }
--- server_test.go
+++ server_test.go
@@ -1,1 +1,21 @@
 package gsrv
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestService(t *testing.T) {
+	r := strings.NewReader(`GET /README.md HTTP/1.0
+Connection: Close
+Content-Type: text/plain
+Content-Length: 100
+`)
+	buf := bytes.Buffer{}
+	service(r, &buf, ".")
+	assert.Contains(t, buf.String(), "HTTP/1.0 200 OK")
+	assert.Contains(t, buf.String(), "# gsrv")
+}
